762041cb4637094aa1fd21b7220ea453382f24d9
gqueiroz <gribeiro.mg@gmail.com>
2020-03-09 08:19:37 -0300

    minor adjustments in setup.py

--- setup.py
+++ setup.py
@@ -52,8 +52,8 @@ install_requires = [
 
 packages = find_packages()
 
+g = {}
 with open(os.path.join('bdc_stac', 'version.py'), 'rt') as fp:
-    g = {}
     exec(fp.read(), g)
     version = g['__version__']
 
