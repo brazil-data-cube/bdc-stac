5886120f40e269f4d2f0f897d2ef11d476a15fc7
Matheus C. Zaglia <mzaglia@gmail.com>
2020-04-01 18:23:11 -0300

    Fix missing docstring

--- bdc_stac/zappa_init.py
+++ bdc_stac/zappa_init.py
@@ -1,3 +1,11 @@
+#
+# This file is part of bdc-stac.
+# Copyright (C) 2019 INPE.
+#
+# bdc-stac is a free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""File used for zappa deploy."""
 from . import create_app
 
 app = create_app()
