23a0f5c43b4ecd24e3df63ad299bf83f0d66d052
Beto Noronha <beto_noronha@live.com>
2020-08-12 09:31:46 -0300

    change SQLALCHEMY_DATABASE_URI

--- bdc_stac/config.py
+++ bdc_stac/config.py
@@ -1,11 +1,21 @@
+#
+# This file is part of bdc-stac.
+# Copyright (C) 2019 INPE.
+#
+# bdc-stac is a free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Config module."""
+
 import os
 
 BDC_STAC_BASE_URL = os.getenv('BDC_STAC_BASE_URL', 'http://localhost:5005')
 BDC_STAC_API_VERSION = os.getenv('BDC_STAC_API_VERSION', '0.8.1')
 BDC_STAC_FILE_ROOT = os.getenv('BDC_STAC_FILE_ROOT', 'http://localhost:5001')
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'),
-                                                               os.getenv('DB_PASS'),
-                                                               os.getenv('DB_HOST'),
-                                                               os.getenv('DB_PORT'),
-                                                               os.getenv('DB_NAME'))
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 
+                                    'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER'),
+                                                                        os.getenv('DB_PASS'),
+                                                                        os.getenv('DB_HOST'),
+                                                                        os.getenv('DB_PORT'),
+                                                                        os.getenv('DB_NAME')))
